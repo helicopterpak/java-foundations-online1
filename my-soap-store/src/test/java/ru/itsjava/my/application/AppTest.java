6026b6b6b9697bb4693ec1976d5cfff0f6c5d702
Pak <helicopterpak@gmail.com>
2021-11-18 11:15:29 +0300

    fixed again

--- my-soap-store/src/test/java/ru/itsjava/my/application/AppTest.java
+++ my-soap-store/src/test/java/ru/itsjava/my/application/AppTest.java
@@ -17,11 +17,9 @@ public class AppTest {
     @Test
     public void shouldHaveCorrectMethodHasSoap () {
         Soap soap1 = new Soap("Nivea", "Body");
-        Soap soap2 = new Soap("Dove", "Hand");
         soapList.add(soap1);
-        soapList.add(soap2);
         SoapService soapService = new SoapServiceImpl(soapList);
-        Assertions.assertTrue(soapService.hasSoap("Dove")); //корректно
+        Assertions.assertTrue(soapService.hasSoap("Nivea")); //корректно
        // Assertions.assertTrue(soapService.hasSoap("Hulu")); //некорректно
     }
 
@@ -30,13 +28,12 @@ public class AppTest {
     public void shouldHaveCorrectMethodPutSoap(){
         Soap soap1 = new Soap("Nivea", "Body");
         Soap soap2 = new Soap("Dove", "Hand");
-        soapList.add(soap1);
 
         SoapService soapService = new SoapServiceImpl(soapList);
         soapService.putSoap(soap2);
 
-        Assertions.assertEquals(soap2,soapList.get(1)); //корректно
-        //Assertions.assertEquals(soap1,soapList.get(1)); //некорректно
+        Assertions.assertEquals(soap2,soapList.get(0)); //корректно
+        //Assertions.assertEquals(soap1,soapList.get(0)); //некорректно
     }
 
     @DisplayName("должен корректно находить позицию по названию и удалять позицию из списка")
@@ -48,11 +45,11 @@ public class AppTest {
         soapList.add(soap2);
 
         SoapService soapService = new SoapServiceImpl(soapList);
-        soapService.takeSoapByFirm("Hulu");//на несовпадение
-        //soapService.takeSoapByFirm("Dove");       //на совпадение
+        //soapService.takeSoapByFirm("Hulu");//на несовпадение
+        soapService.takeSoapByFirm("Dove");       //на совпадение
 
-        Assertions.assertNotEquals("Dove", null);// тест на несовпадение
-       // Assertions.assertEquals("Dove", "Dove");// на совпадение
+        //Assertions.assertNotEquals("Dove", null);// тест на несовпадение
+        Assertions.assertEquals(soap2, soapService.takeSoapByFirm("Dove"));// на совпадение
         System.out.println(soapList.toString());
     }
 }
